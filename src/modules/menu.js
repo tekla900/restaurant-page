9aadff7f72aa54e8bf68589c19e8af42ab6d3f68
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-11-28 17:27:18 +0400

    working on welcome section

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,5 +1,5 @@
 const menu = `
-<section class='flex-column tab' id='menu'>
+    <section class='flex-column tab' id='menu'>
         <div class='flex column-center'>
             <h2>Our Special Dishes</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>    
@@ -7,23 +7,43 @@ const menu = `
         <div class="menu flex space-around">
             <div class="menu-item column-center center">
                 <img src="../assets/dish-1 1.png" class='meal'>
+                <div class='price'>$12</div>
                 <h4>Lumpia with Suace</h4>
                 <p>Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor </p>
             </div>
             <div class="menu-item column-center center">
                 <img src="../assets/dish-3 1.png" class='meal'>
+                <div class='price'>$12</div>
                 <h4>Fish and Veggie</h4>
                 <p>Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor </p>
             </div>
             <div class="menu-item column-center center">
                 <img src="../assets/dish-5.png" class='meal'>
+                <div class='price'>$12</div>
                 <h4>Tofu Chili</h4>
                 <p>Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor </p>
             </div>
             <div class="menu-item column-center center">
                 <img src="../assets/dish-4.png" class='meal'>
+                <div class='price'>$12</div>
                 <h4>Egg and Cocumber</h4>
                 <p>Lorem ipsum dolor sit , consectetur adipiscing elit, sed do eiusmod tempor </p>
+            </div>
+        </div>
+    </section>
+
+    <section class='flex space-around'>
+
+        <div>
+            <img src='../assets/dish-2 2.png' />
+        </div>
+
+        <div class='flex column-justify-center'>
+            <h2>Wecome to Our Restaurant</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div class="btn-container flex">
+                <button type="button" class='black'>Menu</button>
+                <button type="button" class='orange'>Book a table </button>
             </div>
         </div>
     </section>
